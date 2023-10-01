432a6a0a963a053ff6524db3153fa73d35a59ae4
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2023-10-01 09:49:37 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    ex25(12);
+    // expected output:
+    // 1
+    // 2
+    // 3
+    // 4
+    // 6
+    // 12
   }
+  
+  public static void ex25(int n) {
+    
+  }
+  
 }
